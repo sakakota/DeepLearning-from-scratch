a7df395bce62bdd9559ccd8769b57211e727e257
sakakota <kkamo01690@gmail.com>
2022-04-12 05:07:51 +0900

    add relu func

--- Graphs.py
+++ Graphs.py
@@ -1,9 +1,18 @@
 import numpy as np
 import matplotlib.pylab as plt
 from activeFunc import step_function
+from activeFunc import sigmoid
 
+# step関数の描画
 x = np.arange(-5.0, 5.0, 0.1)
 y = step_function(x)
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1)
+plt.show()
+
+# sigmoid関数の描画
+x = np.arange(-5.0, 5.0, 0.1)
+y = sigmoid(x)
 plt.plot(x, y)
 plt.ylim(-0.1, 1.1)
 plt.show()
--- activeFunc.py
+++ activeFunc.py
@@ -6,3 +6,6 @@ def step_function(x):
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
+
+def relu(x):
+    return np.maximum(0, x)
