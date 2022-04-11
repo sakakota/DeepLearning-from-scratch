216143b306e1f9e6b44982f7f8ddcbc221e62d90
sakakota <kkamo01690@gmail.com>
2022-04-12 04:45:52 +0900

    first day commit

---
+++ Graphs.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pylab as plt
+from activeFunc import step_function
+
+x = np.arange(-5.0, 5.0, 0.1)
+y = step_function(x)
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1)
+plt.show()
